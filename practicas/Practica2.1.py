ec1696e7f8968e3a7ab4bae0505b6ccb9211ba4d
westernsquad <guillermo_navas_garciaexp@hotmail.com>
2018-12-05 18:38:35 +0100

    practica2.1

--- practicas/Practica2.1.py
+++ practicas/Practica2.1.py
@@ -2,46 +2,77 @@
 import numpy as np
 import collections
 
-Point = collections.namedtuple("Point",["x","y"])
-Recta = collections.namedtuple('Recta',[ 'uno' , 'dos'])
+Point = collections.namedtuple("Point", ["x", "y"])
+Recta = collections.namedtuple('Recta', ['uno', 'dos'])
 
-Rectangle = collections.namedtuple("Rectangle",["min","max"])
-Triangulo = collections.namedtuple('Triangulo',['iz','cen','der'])
+Triangulo = collections.namedtuple('Triangulo', ['v0', 'v1', 'v2'])
+Cuadrilatero= collections.namedtuple('Cuadrilaterp',['t1','t2'])
+
+
 
 def borde (recta, punto):
-    return (punto.x - recta.uno.x) * (recta.dos.y - recta.uno.y) - (punto.y - recta.uno.y) * (recta.dos.x - recta.uno.x)
+    return  (punto.x - recta.uno.x) * (recta.dos.y - recta.uno.y) - (punto.y - recta.uno.y) * (recta.dos.x - recta.uno.x)
 
 
 def inTriangle (trinagulo,punto):
-    #return (borde(trinagulo.iz,punto) >= 0) and (borde(trinagulo.cen,punto) >= 0) and (borde(trinagulo.der,punto) >= 0)
-    e=borde(trinagulo.iz,punto)
-    e2=borde(trinagulo.cen,punto)
-    e3=borde(trinagulo.der,punto)
-    print('primero'+ str(e) )
-    print('segundoo' + str(e2) )
-    print('terceroo' + str(e3) )
+    return (borde(trinagulo.v0,punto) <= 0) and (borde(trinagulo.v1,punto) <= 0) and (borde(trinagulo.v2,punto) <= 0)
 
-screen = np.zeros((10,10))
-print (screen)
+def rasterice(screen,t,value):
+    f,c=screen.shape
+    for i in range(f):
+        for j in range(c):
+            p=Point(j, i)
+            if inTriangle(t,p):
+                screen[i][j]=value
+#*******************************************poligono
+def poligono (p0,p1,p2,p3):
+    t1=Triangulo(p0,p1,p2)
+    t2=Triangulo(p2,p3,p0)
+    c=Cuadrilatero(t1,t2)
+    return t1,t2
 
-'''changepixel(screen,2,3,4)
-print(screen)
-
-clearscreen(screen)
-print (screen)'''
-
-prueba=Point(0,0)
-p = Point(0, 0)
-q = Point(3,0)
-s = Point(3,3)
+def rastericeP(screen,t1,t2,value):
+    f,c=screen.shape
+    for i in range(f):
+        for j in range(c):
+            p=Point(j, i)
+            if inTriangle(t1,p) or inTriangle(t2,p):
+                screen[i][j]=value
 
 
-r =Recta(p, q)
-r1 =Recta(q, s)
-r2 =Recta(s, p)
-t= Triangulo(r,r1,r2)
-borde(r2,prueba)
-print(inTriangle(t,prueba))
+if __name__ == "__main__":
+    screen = np.zeros((10,10))
+    print (screen)
+
+    prueba=Point(7, 4 )
+    p = Point(2, 4)
+    q = Point(5, 3)
+    s = Point(5, 6)
+    l = Point(2, 6)
+
+
+    r =Recta(p, q)
+    r1 =Recta(q, s)
+    r2 =Recta(s, p)
+    r11=Recta(s,l)
+    r22=Recta(l,p)
+    r33=Recta(p,s)
+    t= Triangulo(r,r1,r2)
+    t2= Triangulo(r11,r22,r33)
+    #pol=poligono(p,q,s,l)
+
+
+    #tt= Triangulo(p,q,s)
+    print (borde(r,prueba))
+    print (borde(r1,prueba))
+    print (borde(r2,prueba))
+    print(inTriangle(t,prueba))
+    rasterice(screen,t,4)
+    print (screen)
+    #rastericeP(screen, t,t2,4)
+    rastericeP(screen, t,t2, 4)
+    print (screen)
+
 
 
 
